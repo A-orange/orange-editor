458bc5a10fd20e0ce682770f636906139d1488fa
A_orange <1504889935@qq.com>
2022-10-19 22:51:10 +0800

    代码高亮

--- src/components/code-editor/CodeEditor.vue
+++ src/components/code-editor/CodeEditor.vue
@@ -3,37 +3,75 @@
     <el-scrollbar :noresize="false" always>
       <div class="orange-code-editor">
         <div class="orange-code-editor-header">
-          <span class="language">{{ codeEditor.language }}</span>
+          <span class="header-tip">
+            <el-autocomplete
+                class="code-language"
+                v-model="codeEditor.language"
+                :fetch-suggestions="fetchLanguage"
+                :trigger-on-focus="false"
+                size="small"
+                popper-class="code-language-popper"
+            />
+          </span>
+          <span class="header-tip" @click="codeEditor.lock = !codeEditor.lock">
+            <el-icon v-if="!codeEditor.lock" :size="15" color="#c2c2c2"><Unlock/></el-icon>
+            <el-icon v-else :size="15" color="#c2c2c2"><Lock/></el-icon>
+          </span>
         </div>
         <div v-if="codeEditor.showLineNumber" class="line-number">
           <div
               class="number"
               :style="lineNumberStyle"
-              v-for="idx in 20"
+              v-for="idx in lineNumber"
           >{{ idx }}
           </div>
         </div>
-        <Editor v-model:value="codeEditor.html" class="code-view" :style="codeEditorStyle"/>
+        <div
+            v-html="codeEditor.lock ? codeEditor.html : codeEditor.code"
+            class="code-view"
+            :style="codeEditorStyle"
+            :contenteditable="!codeEditor.lock"
+            @blur="codeEditor.code=$event.target.innerText"
+        />
       </div>
     </el-scrollbar>
   </div>
 </template>
 
 <script setup lang="ts">
-import {computed, reactive, ref, watch} from "vue";
+import {computed, reactive, watch} from "vue";
 import {editorFonts} from "./fonts/font";
-import Editor from "./Editor.vue";
-import highlight from "highlight.js";
+import {Prism} from "./prism.js";
 
 const codeEditor = reactive({
   fontSize: 14,
   showLineNumber: true,
-  language: 'Vue',
+  language: 'js',
   wordBreak: true, // 换行
   ligatures: true, // 连字
+  lock: true, // 锁
   fontFamily: 'JetBrains Mono ExtraLight', // 默认字体
-  html: ``
+  html: '',
+  code: ''
 })
+// 行数
+const lineNumber = computed(() => {
+  return codeEditor.code ? codeEditor.code.split('\n').length : 1;
+})
+// 语言
+const languages = computed(() => {
+  return Object.keys(Prism.languages).map(key => {
+    return {value: key, label: key}
+  })
+})
+// 过滤语言
+const fetchLanguage = (query: string, cb: any) => {
+  const filterOptions = languages.value.filter(one => {
+    return one.value.toLowerCase().indexOf(query.toLowerCase()) === 0;
+  })
+  cb(filterOptions)
+}
+
 // 行号样式
 const lineNumberStyle = computed(() => {
   return {
@@ -52,15 +90,13 @@ const codeEditorStyle = computed(() => {
     fontFamily
   }
 })
+
 watch(
-    () => codeEditor.html,
-    (html) => {
-      const obj = highlight.highlight(html, {
-        language: 'js'
-      })
-      console.log(obj.value)
-      // codeEditor.html = obj.value;
-    }
+    () => codeEditor.code,
+    (code) => {
+      codeEditor.html = Prism.highlight(code, Prism.languages[codeEditor.language]);
+    },
+    {immediate: true}
 )
 
 </script>
@@ -75,6 +111,7 @@ export default defineComponent({
 
 <style lang="less" scoped>
 @import "styles.less";
+@import "./themes/code-styles.less";
 
 .orange-code-editor-wrap {
   width: 100%;
@@ -99,16 +136,31 @@ export default defineComponent({
       display: flex;
       justify-content: flex-end;
 
-      > .language {
+      > .header-tip {
         font-size: 12px;
         line-height: 20px;
-        padding: 0 5px;
+        padding: 0 8px;
         border-radius: 3px;
         color: @orange-code-editor-language-color;
         cursor: pointer;
+        margin: 0 5px;
+        display: flex;
+        align-items: center;
 
         &:hover {
           background-color: @orange-code-editor-language-color-hover;
+        }
+
+        > .code-language {
+          width: 80px;
+
+          > .el-input > .el-input__wrapper {
+            box-shadow: 0 0;
+
+            > .el-input__inner {
+              text-align: center;
+            }
+          }
         }
       }
     }
@@ -128,11 +180,44 @@ export default defineComponent({
       height: auto;
       flex-shrink: 1;
       flex-grow: 1;
+      user-select: text;
+      white-space: pre-wrap;
+      text-align: left;
+
+      .js();
+
+      &[contenteditable=true] {
+        user-modify: read-write-plaintext-only;
+
+        &:empty:before {
+          content: attr(placeholder);
+          display: block;
+          color: #ccc;
+        }
+      }
 
       &:focus-visible {
         outline: none;
       }
     }
+  }
+}
+</style>
+
+<style lang="less">
+@import "../../styles/dark.less";
+
+.code-language-popper {
+  background-color: @auto-complete-popper !important;
+  opacity: 1 !important;
+  margin-top: -10px !important;
+
+  .el-autocomplete-suggestion__wrap {
+    padding: 0;
+  }
+
+  > .el-popper__arrow:before {
+    background-color: @auto-complete-popper !important;
   }
 }
 </style>
